ca2bb3aa042f41ee8042c09f3d1f91bf8c19c0dd
Alex Nask <dev@nasksoft.ru>
2016-09-02 20:57:30 +0300

    refactored adaptive styles

--- src/app/js/main.js
+++ src/app/js/main.js
@@ -1,11 +1,24 @@
 $(document).ready(function () {
 
   $('.fancybox').fancybox();
-  $('.js-top-menu').slicknav();
 
   $('input').click(function () {
     $('input:not(:checked)').parent().removeClass("price-study__plan--selected");
     $('input:checked').parent().addClass("price-study__plan--selected");
   });
 
+  $('.header-menu__link').on('click', function() {
+    $('.top-menu').toggleClass('active');
+  })
+
+
 });
+//
+// $(document).on('click', function(e) {
+//   if (!$(e.target).closest('.top-menu').length) {
+//
+//     console.log($(e.target).closest('.top-menu__link').length)
+//   } else {
+//     $('.top-menu').removeClass('active')
+//   }
+// });
